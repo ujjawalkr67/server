7b5472b6641fa97d4daaf24ee2525b4c6921fb55
Ujjawal kumar <ujjawalkr67@gmail.com>
2023-07-11 19:27:28 +0530

    config file

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    JWT_SECRET : "f5lNSSQNARS/Rq/aLWP1gVJNVXS1c1gmVqxI2tmJLkw=",
+    PASSWORD : "GIsCx7OQMvJdG8xA",
+    ATLAS_URI: "mongodb+srv://ujjawalkr67:GIsCx7OQMvJdG8xA@cluster0.dcvvnju.mongodb.net/?retryWrites=true&w=majority"
+}
